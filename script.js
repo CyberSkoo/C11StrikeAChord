5e1c5c1265e04d463c59dbf490064f234ce23838
CyberSkoo <159735070+CyberSkoo@users.noreply.github.com>
2024-05-04 00:46:47 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+Author: 
+File Name: script.js
+Date: 
+*/
+
+// Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block"){
+        menu.style.display = "none";
+    }else{
+        menu.style.display = "block";
+    }
+}
